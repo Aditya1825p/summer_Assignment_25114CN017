cbddbfd78c57c0532fd1c969e9027009a09c8782
Aditya1825p <kantaditya428@gmail.com>
2026-06-24 14:51:09 +0530

    Day_22

---
+++ Day_22/Question_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0, flag = 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("String is Palindrome.\n");
+    else
+        printf("String is Not Palindrome.\n");
+
+    return 0;
+}
